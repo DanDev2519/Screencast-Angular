dafc3432bbb55df2c5854dc325f849f76958830e
DanDev2519 <ostryakov.danil@yandex.ru>
2021-07-15 20:07:11 +0300

    Метод navigate и navigateByUrl

--- myapp/src/app/user/user.component.ts
+++ myapp/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -12,13 +13,27 @@ export class UserComponent implements OnInit {
   @Input('user1') admin: any;
   @Input() users: any;
 
-  constructor() {
+  constructor(private _router: Router, private _route: ActivatedRoute) {
+    // ActivatedRoute - текущий путь
     // console.log('constructor', this.admin);
   }
 
   // Это хук, который выполняется после передачи всех данных в компоненту
   ngOnInit(): void {
     // console.log('ngOnInit', this.admin);
+  }
+
+  goToUser(userId: number) {
+    // this._router.navigate([ userId ], { relativeTo : this._route});
+
+    this._router.navigate(['users', userId], { skipLocationChange : true}).then(() => {});
+    // эквивалентная запись
+    // this._router.navigateByUrl('users/' + userId, { skipLocationChange : true});
+
+    // skipLocationChange - оставляет неизменным путь в адресной строке
+    // relativeTo - будет добавлять абсоолютную часть пути
+    // в navigateByUrl '/users/' - / ваереди и путь будет от корня
+    // navigateByUrl и  navigate возвращают промис и его тоже можно обрабатывать
   }
 
 }
